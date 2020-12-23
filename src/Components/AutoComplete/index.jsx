a96f704736f7685b9bbc9ca99b8b25ed69c55d32
Manish Thakur <mthakur@deqode.com>
2020-12-23 13:48:31 +0530

    Done basic functionality with suggestions

--- src/Components/AutoComplete/index.jsx
+++ src/Components/AutoComplete/index.jsx
@@ -1,21 +1,70 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import getSuggestions from "../../Api/index";
 
 const AutoComplete = () => {
+    const inputRef = useRef();
+    const [pageState, setPageState] = useState({
+        inputValue: "",
+        options: [],
+        activeIndex: 0,
+        searchedWord: "",
+        showOptions: false,
+    });
 
-  return (
-    <div className="container">            
-      <div className="form-group">
-          <input
-            className="form-control"
-            type="text"
-            spellCheck="false"
-          />
-      </div>
+    const handlePageState = (newStates) => {
+        setPageState((prevStates) => {
+        return {
+            ...prevStates,
+            ...newStates
+        };
+        });
+    }
 
-      <div>Suggestions will be here</div>
-      
-    </div>
-  );
+    useEffect(() => {
+        // Do not show suggestions if input value is empty
+        if (pageState.inputValue !== "") {
+            const lastCharacter = pageState.inputValue.slice(-1);
+            // Do not show suggestions if last character is space
+            if (lastCharacter === " ") {
+              handlePageState({ options: [], activeIndex: 0 });
+              return;
+            }
+  
+            // Find suggestions based on last word
+            const inputs = pageState.inputValue.split(" ").filter((word) => word !== "");
+            const lastWordFromInput = inputs.pop();
+            
+            handlePageState({ searchedWord: lastWordFromInput });
+            getSuggestions(lastWordFromInput).then((suggestions) => {
+              handlePageState({ options: suggestions, activeIndex: 0, showOptions: true });
+            }).catch(console.error);
+        } else {
+            handlePageState({ options: [], activeIndex: 0 });
+        }
+    }, [pageState.inputValue, inputRef]);
+
+    return (
+        <div className="container">
+            <div className="form-group">
+                <input
+                    className="form-control"
+                    type="text"
+                    spellCheck="false"
+                    ref={inputRef}
+                    value={pageState.inputValue}
+                    onChange={event => handlePageState({ inputValue: event.target.value })}
+                />
+            </div>
+
+            {pageState.showOptions && pageState.options.length !== 0 && (
+                <div>
+                    {pageState.options.map((option) => (
+                        <div key={option}>{option}</div>
+                    ))}
+                </div>
+            )}        
+        </div>
+    );
 }
 
 export default AutoComplete;
