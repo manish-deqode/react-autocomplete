78ae836f6a19b7f3611bc64359c87d7898a75124
Manish Thakur <mthakur@deqode.com>
2020-12-23 14:54:58 +0530

    add styling & select option functionality

--- src/Components/AutoComplete/index.jsx
+++ src/Components/AutoComplete/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import getSuggestions from "../../Api/index";
+import styles from "./index.module.css";
 
 const AutoComplete = () => {
     const inputRef = useRef();
@@ -43,24 +44,84 @@ const AutoComplete = () => {
         }
     }, [pageState.inputValue, inputRef]);
 
+    // Highlights the matched characters in suggestions
+    const highlightOption = (option) => {
+        return option.replace(
+        new RegExp(pageState.searchedWord, "gi"),
+        (match) => `<span class="${styles.highlight}">${match}</span>`
+        );
+    };
+
+    // Selecting suggestions through key press
+    const handleKeyPress = (event) => {
+        const { activeIndex, options } = pageState;
+        if (event.keyCode === 13) { //Enter key
+            onSelectOption(options[activeIndex]);
+        } else if (event.keyCode === 38) { //Up Arrow key
+            event.preventDefault();
+            if (activeIndex === 0) {
+                return;
+            }
+            handlePageState({ activeIndex: activeIndex - 1 });
+        } else if (event.keyCode === 40) { //Down Arrow key
+            event.preventDefault();
+            if (activeIndex === options.length - 1) {
+                return;
+            }
+            handlePageState({ activeIndex: activeIndex + 1 });
+        }
+    };
+
+    // On select suggestion
+    const onSelectOption = (inputOption) => {
+        // If API does not return any result and value is selected, setting the typed value with space
+        if (inputOption === undefined) {
+          handlePageState({ inputValue: (pageState.inputValue !== "" && pageState.inputValue.slice(-1) !== " ") ? pageState.inputValue.concat(" ") : pageState.inputValue });
+        } else {
+          // Append suggestion and update input value
+          const option = inputOption + " ";
+    
+          let updatedValue = pageState.inputValue;
+          if (pageState.inputValue !== "") {
+            const words = pageState.inputValue.split(" ").slice(0, -1);
+            words.push(option);
+            updatedValue = words.join(" ");
+          }
+          handlePageState({ inputValue: updatedValue, activeIndex: 0, options: [] });
+        }
+    
+        // Set focus to input on select
+        if (inputRef && inputRef.current) {
+          inputRef.current.focus();
+        }
+    };
+
     return (
-        <div className="container">
+        <div className={styles.searchContainer}>
             <div className="form-group">
                 <input
-                    className="form-control"
+                    className={`form-control ${styles.inputSearch}`}
                     type="text"
                     spellCheck="false"
                     ref={inputRef}
                     value={pageState.inputValue}
                     onChange={event => handlePageState({ inputValue: event.target.value })}
+                    onKeyDown={handleKeyPress}
                 />
             </div>
 
             {pageState.showOptions && pageState.options.length !== 0 && (
-                <div>
-                    {pageState.options.map((option) => (
-                        <div key={option}>{option}</div>
-                    ))}
+                <div className={styles.optionsContainer}>
+                {pageState.options.map((option, index) => (
+                    <div
+                    key={option}
+                    className={
+                        index === pageState.activeIndex ? styles.activeOption : styles.option
+                    }
+                    onClick={() => onSelectOption(option)}
+                    dangerouslySetInnerHTML={{ __html: highlightOption(option) }}
+                    />
+                ))}
                 </div>
             )}        
         </div>
